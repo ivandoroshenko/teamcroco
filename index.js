610f991fc026bb5a450368b04ff112bba8b7c6fd
ivandoroshenko <ivandoroshenko1988@gmail.com>
2018-06-12 10:58:25 +0300

    added comments

--- index.js
+++ index.js
@@ -8,8 +8,11 @@
 // @grant none 
 // ==/UserScript==  
 
-//create style tag 
+//add validation to the input
 $('input').attr("required", true)
+//inject font awesome stylesheet
 $('head').append('<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">');
+// append style tag to the end of head with custom styling
 $('head').append('<style type="text/css" id="userStyle"></style>');
+// styling input onblur
 $("#company").focusout(function () { $('.controls').append('<i class="fas fa-check"></i>');$('#userStyle').append('#company:valid {background-color: lightgreen}'); $('#userStyle').append('.controls {position: relative}');$('#userStyle').append('.fa-check {position: absolute; right: 10px; top: 50%; transform: translateY(-50%)}');});
