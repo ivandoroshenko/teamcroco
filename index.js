946013b3d92e9551033d1ce25f82e2eabb2e210b
ivandoroshenko <ivandoroshenko1988@gmail.com>
2018-06-13 07:51:14 +0300

    validation

--- index.js
+++ index.js
@@ -8,11 +8,20 @@
 // @grant none 
 // ==/UserScript==  
 
+// sorry for long lines! Console does not accept multiline command :_(
+
 //add validation to the input
 $('input').attr("required", true)
 //inject font awesome stylesheet
 $('head').append('<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">');
 // append style tag to the end of head with custom styling
 $('head').append('<style type="text/css" id="userStyle"></style>');
-// styling input onblur
-$("input").focusout(function () {$('#controls').append('<i class="fas fa-check"></i>');$('#userStyle').append('#company:valid {background-color: lightgreen}'); $('#userStyle').append('.controls {position: relative}');$('#userStyle').append('.fa-check {position: absolute; right: 10px; top: 50%; transform: translateY(-50%)}');});
+
+// get all necessary inputs inside the fieldset
+var inputList = $$('fieldset input');
+// put inputs into the loop, get their "id" and set them as classes to parent "divs" 
+for (var i = 0; i <= inputList.length; i++) {var input = $(inputList)[i];var inputId = $(input).attr('id'); var inputParent = $(input).parent(); var inputParentClass = $(inputParent).attr('class'); inputParentClass = inputId; $('input').focus(function () {if ($(input).closest('fieldset'))});}
+
+    // styling input onblur
+    $("#company").focusout(function () {var parent = $(this).parent.css();$('.controls').append('<i class="fas fa-check"></i>');$('#userStyle').append('#company:valid {background-color: lightgreen}'); $('#userStyle').append('.controls {position: relative}');$('#userStyle').append('.fa-check {position: absolute; right: 10px; top: 50%; transform: translateY(-50%)}');});
+    $('input').focus(function () { if ($(this).closest('fieldset')) { var inputId = $(this).attr('id'); var inputParent = $(this).parent(); var inputParentClass = $(inputParent).attr('class'); inputParentClass = inputId; console.log(inputParentClass)}})
